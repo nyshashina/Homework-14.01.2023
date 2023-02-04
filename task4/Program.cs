3e00b11f2600190ed7f3b19984bf5019568a610a
nyshashina <nyshashina@gmail.com>
2023-02-04 13:01:03 +0300

    task4 is done

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine("Введите число");
+int numberN = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Четные числа от 1 до " + numberN + ":");
+for (int i = 2; i <= numberN; i++){
+    if(i % 2 == 0){
+        Console.Write(i + " ");
+    }
+}

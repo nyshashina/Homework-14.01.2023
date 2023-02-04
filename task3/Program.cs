d48bfaf9499ce0854190f222e9c1a1f741ac6d9f
nyshashina <nyshashina@gmail.com>
2023-02-04 12:49:28 +0300

    task3 is done

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine("Введите число");
+int numberA = int.Parse(Console.ReadLine() ?? "0");
+
+if(numberA % 2 == 0){
+    Console.WriteLine("Число " + numberA + " является четным");
+}
+else{
+    Console.WriteLine("Число " + numberA + " не является четным");
+}

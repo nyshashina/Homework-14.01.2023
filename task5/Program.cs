83afbfa19dcf1ed40f8286986151b5e8e2a125c0
nyshashina <nyshashina@gmail.com>
2023-02-04 13:08:09 +0300

    task5 is done

---
+++ task5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число");
+int numberA = int.Parse(Console.ReadLine() ?? "0");
+
+if(numberA >= 100 && numberA <= 999){
+    Console.WriteLine("Последняя цифра трехзначного числа " + numberA % 10);
+}
+else{
+    Console.WriteLine("Это не трехзначное число.");
+}

afbe5944ac609ef34006767fac9e2698aeb7e215
nyshashina <nyshashina@gmail.com>
2023-02-04 12:30:20 +0300

    task1 is done

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+Console.WriteLine("Введите первое число");
+int numberA = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите второе число");
+int numberB = int.Parse(Console.ReadLine() ?? "0");
+
+if(numberB * numberB == numberA) {
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else {
+    Console.WriteLine("Первое число не является квадратом второго");
+}

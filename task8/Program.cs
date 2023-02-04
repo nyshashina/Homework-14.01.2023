0d432db5f5d8060c4f8d799c7705f81ee890c468
nyshashina <nyshashina@gmail.com>
2023-02-04 13:33:28 +0300

    task8 is done

---
+++ task8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число");
+int numberA = int.Parse(Console.ReadLine() ?? "0");
+
+if(numberA >= 100 && numberA <= 999){
+    int firstA = numberA / 100;
+    int thirdA = numberA % 10;
+    int xA = firstA * 10 + thirdA;
+    Console.WriteLine("Если удалить вторую цифру трёхзначного числа " + numberA + ", получится " + xA);
+}
+else{
+    Console.WriteLine("Это не трехзначное число.");
+}

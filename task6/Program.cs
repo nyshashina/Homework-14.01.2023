3cb44b951fdc8bb155d10984496a721c5913bbb6
nyshashina <nyshashina@gmail.com>
2023-02-04 13:19:06 +0300

    task6 is done

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число");
+int numberA = int.Parse(Console.ReadLine() ?? "0");
+
+if(numberA >= 100 && numberA <= 999){
+    int secondA = numberA / 10;
+    Console.WriteLine("Вторая цифра трехзначного числа " + secondA % 10);
+}
+else{
+    Console.WriteLine("Это не трехзначное число.");
+}
